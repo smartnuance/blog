6822857733ddb15d02ec00ff1be118ad0a9a634b
Simon Wehrli <simon@smartnuance.com>
2021-09-12 09:37:02 +0100

    change prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "prismjs/themes/prism-solarizedlight.css";
+import "prism-material-themes/themes/material-default.css";
 
 // https://www.gatsbyjs.com/plugins/gatsby-remark-prismjs/?=remark#optional-add-line-numbering
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
